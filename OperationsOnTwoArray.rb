d9712aa5529772e55cb712826f5166ccee75b12e
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 10:47:12 +0530

    Merge remote changes

---
+++ OperationsOnTwoArray.rb
@@ -0,0 +1,28 @@
+arr1=[1,2,3,4,5,6,7]
+arr2=[7,8,9,10,11,23]
+
+puts arr1-arr2
+
+puts "\n"
+
+puts arr1+arr2
+
+puts "\n"
+
+puts arr2.join
+
+puts "\n"
+
+puts arr1|arr2
+
+puts "\n"
+
+puts arr2|arr1
+
+puts "\n"
+
+puts arr1&arr2
+
+puts "\n"
+
+puts arr2&arr1
