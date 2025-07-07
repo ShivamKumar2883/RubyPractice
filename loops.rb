dcf1cdcd3190997c3ad9cfbd4260ae60a28f7f04
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 09:40:02 +0530

    Your commit message

--- loops.rb
+++ loops.rb
@@ -7,3 +7,26 @@ end
 1.upto(10) do |x|
     print x
 end
+
+#Their are 10 cases and types of loop statements
+# 1. while loop
+# 2. Until loop
+# 3. for loop
+# 4. each loop
+# 5. times loop
+# 6. upto/downto loops
+# 7. loop(infinite) or loop with break
+# 8. step loop 
+# 9. nested loops
+# 10. loop control
+
+# When to Use Which:
+# Use each for collections (most Ruby-like)
+
+# Use times for fixed iterations
+
+# Use while/until for conditional looping
+
+# Use for when coming from other languages
+
+# Use step for custom increments
