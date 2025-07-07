462b7931bca2f774b1979e0d7e8328469282e7ff
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 12:45:42 +0530

    Merge remote changes

---
+++ WorkingOfReturn.rb
@@ -0,0 +1,16 @@
+a = 3
+b = 2 
+def add(a,b)
+a+b
+end
+puts(add(a,b))
+
+
+a = 1
+b = 2 
+def add(a,b)
+return(a+b)
+end
+puts(add(a,b))
+
+#You don't need return statement in ruby for last line function!
