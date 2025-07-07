bbe45931d1fe05b6e61eed13d6499c33ee173e12
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 08:04:32 +0530

    Initial commit with hello.rb

---
+++ loops.rb
@@ -0,0 +1,9 @@
+for i in 1..10
+  puts i
+end
+
+3.times{print "Ruby!"}
+
+1.upto(10) do |x|
+    print x
+end
---
+++ sum.rb
@@ -0,0 +1,11 @@
+
+a = 1
+b = 2 
+c = a+b
+puts "Sum of #{a} and #{b} = #{c}"
+
+def add(a,b)
+    return a+b
+end
+result = add(2,3)
+puts result
