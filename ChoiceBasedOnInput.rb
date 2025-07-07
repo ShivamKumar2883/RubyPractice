f147a11aa302c7bd94e96feba33e3e661ed64fb7
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 12:13:56 +0530

    Merge remote changes

---
+++ ChoiceBasedOnInput.rb
@@ -0,0 +1,17 @@
+puts "Enter a number"
+number = gets.to_i
+
+case number
+when 1 then
+  puts "One"
+when 2 then
+  puts "Two"
+when 3 then
+  puts "Three"
+when 4 then
+  puts "Four"
+when 5 then
+  puts "Five"
+else
+  puts "Between 1 to 5 only"
+end
