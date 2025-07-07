15086c980f77e05e46a6dc0c8783aac053547663
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 09:33:55 +0530

    Your commit message

---
+++ basicStringFunctions.rb
@@ -0,0 +1,26 @@
+s= "Hello Shivam"
+
+puts s
+
+puts s.length
+
+# To Chnage in upperCase
+puts s.upcase
+
+#To lowerCase
+puts s.downcase
+
+#to Reverse
+puts s.reverse
+
+#to check string is empty or not
+puts "".empty?
+
+
+#Output
+# Hello Shivam
+# 12
+# HELLO SHIVAM
+# hello shivam
+# mavihS olleH
+# true
