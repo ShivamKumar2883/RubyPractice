5274e2497d64449e9c112b3bd4e64634f655d658
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-07 10:14:51 +0530

    Your commit message

---
+++ array.rb
@@ -0,0 +1,19 @@
+arr =[]
+
+arr=[1,2,3,4,5,"Seven"]
+
+#we can use %w for string array
+words=%w[shivam satyam vamika]
+
+#without %w
+
+fruits = ["apple", "bannana", "orange"]
+puts fruits[0]
+puts fruits[1]
+puts fruits[-1]
+puts fruits[2]
+
+nums = [0, 1, 1.5, 2, 3, 4]
+
+nums.push(5)
+puts nums[6]
