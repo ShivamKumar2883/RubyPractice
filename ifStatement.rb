a26acb6d76b29735fef4e6558d8d32191b9e977a
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-08 13:55:30 +0530

    Merge remote changes

---
+++ ifStatement.rb
@@ -0,0 +1,17 @@
+x = 2
+if x<5 
+    puts "Number is less the 5"
+elsif x>5
+    puts "Number is gretater than 5"
+else
+    puts "HELLO"
+end
+
+
+if x>5
+    puts "x is greater then 5"
+elsif x<5
+    puts "x is SMALLER than 5"
+else 
+    puts "x is eiqal to5"
+end
