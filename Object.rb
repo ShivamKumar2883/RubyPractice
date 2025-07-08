294f9ec4ec76f1a8ebe4c9a58294f471092d90ac
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-08 10:54:50 +0530

    Merge remote changes

---
+++ Object.rb
@@ -0,0 +1,44 @@
+#Object Refrences
+s ="Hello"
+t = s
+t[-1]="Vijay"
+puts s
+t = "Ruby"
+puts s,t
+
+#Object identifier
+a=45
+puts a.object_id
+puts a.__id__
+
+#Object Class and Object Type
+o = "tets"
+puts o.class
+puts o.class.superclass.superclass
+puts o.class.superclass.superclass.superclass
+puts o.class == String
+puts o.instance_of? String
+
+x=1
+puts x.instance_of?(Integer) 
+puts x.instance_of?(Numeric) 
+puts x.is_a?(Integer)             
+puts x.is_a?(Numeric)
+puts x.is_a?(Object)
+
+# Output:
+# HellVijay
+# HellVijay
+# Ruby
+# 91
+# 91
+# String
+# BasicObject
+
+# true
+# true
+# true
+# false
+# true
+# true
+# true
