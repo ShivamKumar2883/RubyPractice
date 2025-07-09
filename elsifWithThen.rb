d6edb49433aac599f3bd020c1c42daa1d14fab05
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 09:35:02 +0530

    Merge remote changes

---
+++ elsifWithThen.rb
@@ -0,0 +1,13 @@
+x = 2 
+name = if x == 1 then "one"
+    elsif x == 2 then "two"
+    elsif x == 3 then "three"
+    elsif x == 4 then "four"
+    else "many"
+end
+puts name
+
+#if as a modifiyer
+x = 5
+puts x if x>2
+
