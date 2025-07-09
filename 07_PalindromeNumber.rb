dbc2421ae85b0a3537e128a44e68a9ceb496d89d
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 14:17:39 +0530

    Merge remote changes

---
+++ 07_PalindromeNumber.rb
@@ -0,0 +1,14 @@
+# WAP to accept a number and check it is palindrome or not?
+
+num = gets.to_i
+rev=0
+m=num
+while (num>0)
+    u=num%10
+    rev=(rev*10)+u
+    num=num/10
+end
+if(m==rev) then puts "Given number #{m} is Palindrome Number."
+else puts "Given number #{m} is not a palindrome Number."
+end
+
