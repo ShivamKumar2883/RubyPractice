ba7aa8b4cd0a351b7604a869dd9c0ecb271c9505
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 09:39:33 +0530

    Merge remote changes

---
+++ unless.rb
@@ -0,0 +1,13 @@
+# unless only works if the condition is false
+x = 2
+unless x==5
+    puts "x is smaller and greater than 5"
+end
+
+#unless and else
+x = 5
+unless x==5
+    puts "x is smaller and greater then 5"
+else
+    puts "x is 5"
+end
