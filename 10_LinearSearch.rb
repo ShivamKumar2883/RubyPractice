857382dfbc768849dedb60b71445d894ed5d008b
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 16:34:48 +0530

    Merge remote changes

--- 10_LinearSearch.rb
+++ 10_LinearSearch.rb
@@ -1,14 +1,42 @@
 #WAP to search the number in present arrya.
 
-print "Enter the size of array"
+print "Enter the size of array \n"
 
 n=gets.to_i
 
 array = []
 
 for i in 1..n
-    puts "Enter the elements"
-    array << gets
+    puts "Enter the element for index #{i-1}"
+    array << gets.to_i
 end
 
 puts "You entered: #{array}"
+
+puts "now enter the number to search \n"
+
+sn=gets.to_i
+
+for i in 0..n
+    if(array[i]==sn)
+        puts "The number is present with index #{i}"
+        break
+    end
+end
+
+=begin
+
+OUTPUT:
+Enter the size of array 
+3
+Enter the element for index 0
+1
+Enter the element for index 1
+2
+Enter the element for index 2
+3
+You entered: [1, 2, 3]
+now enter the number to search 
+2
+The number is present with index 1
+=end
