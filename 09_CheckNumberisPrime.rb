e8bfa4722f71edade05c9de6185ac7c91fcb3027
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 15:35:46 +0530

    Merge remote changes

---
+++ 09_CheckNumberisPrime.rb
@@ -0,0 +1,22 @@
+def primeOrNot(num)
+    tf=1
+    for i in 1..num
+        tf += 1 if num % i == 0
+        end
+
+        if(tf==2)
+         return true
+        else
+         return false
+        end
+    end
+
+
+puts "Enter the number to check its prime or not."
+num=gets.to_i
+
+if(primeOrNot(num))
+    puts "Given number #{num} is prime"
+else
+    puts "Given number #{num} is not prime"
+end
