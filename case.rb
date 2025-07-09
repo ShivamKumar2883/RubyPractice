f288dba07a810c24e6efbe4e94222ab2c22956b2
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-09 10:07:05 +0530

    Merge remote changes

---
+++ case.rb
@@ -0,0 +1,8 @@
+x=1
+name = case
+    when x==1
+        "one"
+    when x ==2; "two"
+when x==3 then "three"
+end
+puts name
---
+++ moreOnCase.rb
@@ -0,0 +1,6 @@
+x =2
+y=1
+puts case
+    when x==1, y==0 then "x is one or y is zero"
+    when x==2 || y==1 then "x is two or y is one"
+end
