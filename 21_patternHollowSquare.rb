8d138d6340fced68e784d0d7af47f3c0e1e50f6b
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-10 17:34:06 +0530

    Merge remote changes

---
+++ 21_patternHollowSquare.rb
@@ -0,0 +1,23 @@
+for i in 0..5
+    for j in 0..5
+        if i == 0 || i == 5 || j == 0 || j == 5
+        print "#"
+        else
+            print ' '
+        end
+    end
+    puts
+end
+
+=begin
+
+Result output:
+
+######
+#    #
+#    #
+#    #
+#    #
+######
+
+=end
